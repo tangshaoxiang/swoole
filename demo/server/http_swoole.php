2a58a898dbe8f086e8cf5978cc38c9e33299a821
darian <873246925@qq.com>
2018-06-28 18:42:11 +0800

    t_up

--- demo/server/http_swoole.php
+++ demo/server/http_swoole.php
@@ -7,6 +7,6 @@
  */
 $http = new swoole_http_server("0.0.0.0", 8811);
 $http->on('request', function ($request, $response) {
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>".json_encode($request->get()));
 });
 $http->start();
