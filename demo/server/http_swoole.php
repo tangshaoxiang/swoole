3fce43b559459798719628583c654e37e4c472e6
darian <873246925@qq.com>
2018-07-03 17:27:09 +0800

    t_up

--- demo/server/http_swoole.php
+++ demo/server/http_swoole.php
@@ -1,12 +1,16 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Ty_Ro
- * Date: 2018/6/28
- * Time: 18:26
+ * User: baidu
+ * Date: 18/2/28
+ * Time: 上午1:39
  */
 $http = new swoole_http_server("0.0.0.0", 8811);
-$http->on('request', function ($request, $response) {
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>".json_encode($request->get()));
+
+$http->on('request', function($request, $response) {
+
+    $response->cookie("singwa", "xsssss", time() + 1800);
+    $response->end("sss". json_encode($request->get));
 });
+
 $http->start();
