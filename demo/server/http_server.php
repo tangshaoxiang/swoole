ad2420e199c583a3321cc7fd58ae473c1659fbd6
darian <873246925@qq.com>
2018-07-04 00:37:46 +0800

    t_up

--- demo/server/http_server.php
+++ demo/server/http_server.php
@@ -7,6 +7,13 @@
  */
 $http = new swoole_http_server("0.0.0.0", 8811);
 
+$http->set(
+    [
+        'enable_static_handler' => true,
+        'document_root' => "/home/wwwroot/default/swoole/data",
+    ]
+);
+
 $http->on('request', function($request, $response) {
 
     $response->cookie("singwa", "xsssss", time() + 1800);
